06f13aff65b8c9fadd79e3e69c58990f1aba3f37
Jason Salazar <103016083+Jasonsal23@users.noreply.github.com>
2025-07-19 01:03:40 -0700

    fixed image names

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Image src="/invitation.png" alt="cover" width={1200} height={600} />
+      <Image src="/maininvitation.png" alt="cover" width={1200} height={600} />
       <RSVPForm />
-      <Image src="/logo.jpg" alt="Logo" width={1200} height={600} />
+      <Image src="/mainlogo.jpg" alt="Logo" width={1200} height={600} />
 
     </main>
   );
