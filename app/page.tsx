9777e85ceb59232eac38bc42dd6f7c1874af455b
Jason Salazar <103016083+Jasonsal23@users.noreply.github.com>
2025-07-19 00:25:25 -0700

    moved images to public folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 import RSVPForm from "./_components/RSVPForm";
-import Logo from "./_components/logo.jpg";
-import Invitation from "./_components/invitation.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Image src={Invitation} alt="cover" className="w-full" />
-      
+      <Image src="/invitation.png" alt="cover" className="w-full" width={1200} height={600} />
       <RSVPForm />
-      <Image src={Logo} alt="cover" className="w-full" />
+      <Image src="/logo.jpg" alt="cover" className="w-full" width={1200} height={600} />
     </main>
   );
 }
