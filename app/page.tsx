10219f36fa39797ef6286455721cd8493096def9
Jason Salazar <103016083+Jasonsal23@users.noreply.github.com>
2025-07-19 00:58:00 -0700

    images not loading correctly

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
-      <Image src="/invitation.png" alt="cover" className="w-full" width={1200} height={600} />
+      <Image src="/invitation.png" alt="cover" width={1200} height={600} />
       <RSVPForm />
-      <Image src="/logo.jpg" alt="cover" className="w-full" width={1200} height={600} />
+      <Image src="/logo.jpg" alt="Logo" width={1200} height={600} />
+
     </main>
   );
 }
