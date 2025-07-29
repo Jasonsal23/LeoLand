9676920c4b4fc14bcc39eb6435c5cbe772aba795
Jason Salazar <103016083+Jasonsal23@users.noreply.github.com>
2025-07-29 15:28:55 -0700

    added a link to the /login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import RSVPForm from "./_components/RSVPForm";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Image src="/maininvitation.png" alt="cover" width={1200} height={600} />
       <RSVPForm />
-      <Image src="/mainlogo.jpg" alt="Logo" width={1200} height={600} />
 
+      {/* Clickable logo without <a> */}
+      <Link href="/login">
+        <Image src="/mainlogo.jpg" alt="Logo" width={1200} height={600} />
+      </Link>
     </main>
   );
 }
